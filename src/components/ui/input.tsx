0cb33173732e9e4a69ae39c9dbc3012bfefe3fbf
a3laa wahib <alaawaaheeb@gmail.com>
2026-06-23 10:16:02 +0000

    لا اريد ان يتحرك اي رقم صعودا ونزول علي حسب حركه الماوس - بمعني لما اضغط

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,6 @@
+
+"use client"
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -12,6 +15,12 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
+        onWheel={(e) => {
+          // منع تغيير الأرقام عند تحريك بكرة الماوس (Mouse Wheel)
+          if (type === 'number') {
+            (e.target as HTMLInputElement).blur();
+          }
+        }}
         {...props}
       />
     )
